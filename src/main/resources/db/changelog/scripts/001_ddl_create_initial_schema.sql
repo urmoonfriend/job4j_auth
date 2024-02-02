29ae99dbe5071253f3068832303929fa751fd310
aidos-dev <aidos@paygate.kz>
2024-02-02 18:06:42 +0600

    rm cached

--- src/main/resources/db/changelog/scripts/001_ddl_create_initial_schema.sql
+++
@@ -1,6 +0,0 @@
-create table person
-(
-    id       serial primary key not null,
-    login    varchar(2000) unique,
-    password varchar(2000)
-);
--- src/main/resources/db/changelog/scripts/002_dml_insert_persons.sql
+++
@@ -1,3 +0,0 @@
-insert into person (login, password) values ('parsentev', '123');
-insert into person (login, password) values ('ban', '123');
-insert into person (login, password) values ('ivan', '123');
